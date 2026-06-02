01cfe41e394170a77757d952275993a8e32ad52c
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-03 00:42:58 +0530

    Fix loop to iterate over count items for majority element

--- Majority Element/mysolution.py
+++ Majority Element/mysolution.py
@@ -2,6 +2,6 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count=Counter(nums)
         n=len(nums)
-        for num,count in count.most_common():
+        for num,count in count.items():
             if count>n/2:
                 return num
