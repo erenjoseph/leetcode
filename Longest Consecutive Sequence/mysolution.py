1b5675f87abebeebd429fa76a765e8c722637ed7
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-03 00:29:05 +0530

    Fix longest consecutive sequence logic and return value

--- Longest Consecutive Sequence/mysolution.py
+++ Longest Consecutive Sequence/mysolution.py
@@ -1,8 +1,9 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         s=set(nums)
+        length=0
         longest=0
-        for i in nums:
+        for i in s:
             if i-1 not in s:
                 next_num=i+1
                 length=1
@@ -10,4 +11,4 @@ class Solution:
                     next_num+=1
                     length+=1
                 longest=max(longest,length)
-        return length
+        return longest
