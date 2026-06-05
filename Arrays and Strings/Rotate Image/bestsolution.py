d63753b49d4ee591b4fc2f0edc2d5f1014b8b7a4
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-06 00:03:13 +0530

    Implement matrix rotation in bestsolution.py

---
+++ Arrays and Strings/Rotate Image/bestsolution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        # Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Horizontal reflection
+        for row in matrix:
+            row.reverse()
+       
+
+
+
+        
