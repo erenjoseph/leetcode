4b7f77311d28a2a9d36cb29b4d02652a85e72ec7
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-05 23:44:56 +0530

    Implement spiral order traversal for matrix

--- Arrays and Strings/Spiral Matrix/bestsolution.py
+++ Arrays and Strings/Spiral Matrix/bestsolution.py
@@ -1,1 +1,10 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans=[]
+        while matrix:
+                row=matrix.pop(0)
+                ans.extend(row)
+                matrix = list(zip(*matrix))[::-1]
 
+        return ans
+        
