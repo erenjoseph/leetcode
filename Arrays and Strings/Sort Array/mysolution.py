9518d7aa9ca730157df1bf881dc3aae571a9c929
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-08 01:36:38 +0530

    Create mysolution.py

---
+++ Arrays and Strings/Sort Array/mysolution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums)<=1:
+            return nums
+        mid=len(nums)//2
+        left=nums[:mid]
+        right=nums[mid:]
+        left_sorted=self.sortArray(left)
+        right_sorted=self.sortArray(right)
+        return self.merge(left_sorted,right_sorted)
+
+    def merge(self,left,right):
+        sorted_arr=[]
+        i=j=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                sorted_arr.append(left[i])
+                i+=1
+            else:
+                sorted_arr.append(right[j])
+                j+=1
+        sorted_arr.extend(left[i:])
+        sorted_arr.extend(right[j:])
+
+        return sorted_arr
