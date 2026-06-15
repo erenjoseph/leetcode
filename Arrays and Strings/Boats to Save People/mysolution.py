9c9ad7adff43813c80a5e06733e540cffea477a7
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-16 00:32:27 +0530

    Add solution for 'Boats to Save People' problem
    Implement a solution to calculate the number of rescue boats needed for people with weight limits.

---
+++ Arrays and Strings/Boats to Save People/mysolution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        n=len(people)
+        people.sort()
+        i=0
+        j=n-1
+        boat=0
+        while i<=j:
+            if people[i]+people[j]<=limit:
+                    i+=1
+                    j-=1
+            else:
+                j-=1
+            boat+=1
+        return boat
+                
