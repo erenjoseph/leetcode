021e9a833206f25635331bd92671b5d0ba086a37
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-20 01:39:23 +0530

    Implement carFleet method to calculate fleets

---
+++ Stack/Car Fleet/mysolution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars=[[p,s] for p,s in zip(position,speed)]
+        stk=[]
+        for p,s in sorted(cars)[::-1]:
+            time=(target-p)/s
+            if not stk or stk[-1]<time:
+                stk.append(time)
+        
+        return len(stk)
