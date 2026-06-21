334a0940686d844ca1ddf43a91b9d478492b3aed
Eren <85866515+erenjoseph@users.noreply.github.com>
2026-06-22 02:26:33 +0530

    Implement FreqStack for frequency-based stack operations

---
+++ Stack/Maximum Frequency Stack/mysolution.py
@@ -0,0 +1,22 @@
+from collections import Counter
+class FreqStack:
+
+    def __init__(self):
+        self.heap=[]
+        self.counter=0
+        self.freq=Counter()
+
+    def push(self, val: int) -> None:
+        self.freq[val]+=1
+        self.counter+=1
+        heapq.heappush(self.heap,(-self.freq[val],-self.counter,val))
+    def pop(self) -> int:
+        _,_,x=heapq.heappop(self.heap)
+        self.freq[x]-=1
+        return x
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
